b4ee2e2d601e2880d641045248f7597071f4ff11
riko54321 <s2310131.suzuki.cgp@gmail.com>
2025-12-12 01:13:37 +0900

    nikoniko

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -8,6 +10,8 @@ Rails.application.routes.draw do
 end
 
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   root "application#hello"
